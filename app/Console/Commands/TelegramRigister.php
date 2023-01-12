900aa48f794d8a32149397a5bbf4175a9ba72dd3
farklin <ivanhel@yandex.ru>
2023-01-12 16:38:38 +0300

    правка register

--- app/Console/Commands/TelegramRigister.php
+++ app/Console/Commands/TelegramRigister.php
@@ -63,10 +63,8 @@ class TelegramRigister extends Command
             $MadelineProto = new \danog\MadelineProto\API('session.madeline', $settings);
     
             $MadelineProto->phone_login(env('TELEGRAM_MADELINE_PHONE')); //вводим в консоли свой номер телефона
-            stream_set_blocking(STDIN, false);
-            $code = @trim(fgets(STDIN));
-
-            $authorization = $MadelineProto->complete_phone_login($code); // вводим в консоли код авторизации, который придет в телеграм
+            sleep(60); 
+            $authorization = $MadelineProto->complete_phone_login(config('telegram')['code']); // вводим в консоли код авторизации, который придет в телеграм
     
             if ($authorization['_'] === 'account.noPassword') {
     
